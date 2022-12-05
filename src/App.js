eae6bdb91db906986e3644a86b057b3600725fb2
jin <hot8851@daum.net>
2022-12-05 21:11:07 +0900

    First commit

---
+++ src/App.js
@@ -0,0 +1,59 @@
+import React, {useState, useCallback } from "react";
+import "./App.css";
+import Lists from "./components/Lists";
+import From from "./components/From";
+
+const initialTodoData = localStorage.getItem("todoData") ? JSON.parse(localStorage.getItem("todoData")) : [];
+
+function App() {
+  console.log('App Components');
+  const [todoData, setTodoData] = useState(initialTodoData);
+  const [value, setValue] = useState("");
+
+  const handleSubmit = (e) => {
+    // form 안에 input을 전송할 때 페이지 리로드 되는걸 막아줌
+    e.preventDefault();
+
+    // 새로운 할 일 데이터
+    let newTodo = {
+      id: Date.now(),
+      title: value,
+      completed: false
+    }
+
+    // 원래있던 할일에 새로운 할일 더해주기
+    setTodoData((prev) => [...prev, newTodo]);
+    localStorage.setItem('todoData', JSON.stringify([...todoData, newTodo]));
+    setValue("");
+  };
+
+  const handleClick = useCallback(
+    (id) => {
+      let newTodoData = todoData.filter((data) => data.id !== id);
+      console.log('newTodoData', newTodoData);
+      setTodoData(newTodoData);
+      localStorage.setItem('todoData', JSON.stringify(newTodoData));
+    },
+    [todoData]
+  );
+
+  const handleRemoveClick = () => {
+    setTodoData([]);
+    localStorage.setItem('todoData', JSON.stringify([]));
+  };
+
+  return(
+    <div className="flex items-center justify-center w-screen h-screen bg-blue-100">
+      <div className="w-full p-5 m-4 bg-white rounded shadow lg:w-3/4 lg:max-w-lg">
+        <div className="flex justify-between mb-3">
+          <h1>할 일 목록</h1>
+          <button onClick={handleRemoveClick}>Delete All</button>
+        </div>
+        <Lists handleClick={handleClick} todoData={todoData} setTodoData={setTodoData} />
+        <From handleSubmit={handleSubmit} value={value} setValue={setValue} />
+      </div>
+    </div>
+  )
+}
+
+export default App;
